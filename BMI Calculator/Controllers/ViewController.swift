1b22ecd0e7c259e804e6c2ad808784d57ac6f32a
Abdulbaki <Salausmart132@gmail.com>
2025-07-30 18:47:09 +0300

    link sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -1,15 +1,20 @@
-//
-//  ViewController.swift
-//  BMI Calculator
-//
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HeightLabel: UIStackView!
+    @IBOutlet weak var WeightLabel: UILabel!
+    
+    @IBAction func HeightSliderChange(_ sender: UISlider) {
+        print(String(format: "%.2f", sender.value))
+    }
+    
+    @IBAction func WeightSliderChange(_ sender: UISlider) {
+        print(Int (sender.value))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
